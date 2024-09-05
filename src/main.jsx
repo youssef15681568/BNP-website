0222bb2ee368a4a91e87c81ec90a7c9056ec2567
youssef15681568 <https://github.com/youssef15681568>
2024-09-05 20:36:26 +0100

    05/09/24 8:36

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import './index.css'
 import Home from './pages/homePage'
 import Signin from './pages/auth/signInForm' 
 import Signup from './pages/auth/signUpForm'
-import {BrowserRouter, Router} from 'react-router-dom'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
